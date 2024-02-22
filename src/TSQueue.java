6a934c051d611e17457b61d3f57792eba8431189
cwdatlas <ascott@carroll.edu>
2024-02-21 22:16:34 -0700

    minimized redundant code

--- src/TSQueue.java
+++ src/TSQueue.java
@@ -3,35 +3,27 @@ import java.util.List;
 
 public class TSQueue {
     private final List<Orange> queue = new LinkedList<>();
-    private boolean queueLock = false;
 
     public synchronized Orange pop(){
-        while (queueLock || queue.isEmpty()){
+        while (queue.isEmpty()){
             try{
                 this.wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        queueLock = true;
         Orange orange = queue.remove(0);
-        queueLock = false;
         this.notifyAll();
         return orange;
 
     }
     public synchronized boolean push(Orange orange) {
-        while (queueLock) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        queueLock = true;
         boolean rem = queue.add(orange);
-        queueLock = false;
         this.notifyAll();
         return rem;
+    }
+
+    public synchronized int getLength(){
+        return queue.size();
     }
 }
